d828e0a03ef86c18d7fd952943fccd22ed5be56c
Fejtrix <samkodugas2902@gmail.com>
2026-03-17 17:48:45 +0100

    Update rexlib.lua

--- rexlib.lua
+++ rexlib.lua
@@ -1,6 +1,10 @@
 local RexLib = {}
 
--- 🕒 Get current time (HH:MM:SS)
 function RexLib.getTime()
     return os.date("%H:%M:%S")
+end
+
+function RexLib.inpercent(value,maxvalue)
+local percentage = maxvalue/value*100
+return percentage
 end
