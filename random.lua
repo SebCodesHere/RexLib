cbf6b141f159e7da0aa30fbfdf9b7e351f12bfdf
Fejtrix <samkodugas2902@gmail.com>
2026-03-17 22:07:08 +0100

    Update random.lua

--- random.lua
+++ random.lua
@@ -1,8 +1,9 @@
-local RexLib = require("rexlib")
+--local RexLib = require("rexlib")
 
-print("Current time:", RexLib.getTime())
-print("Percent:", RexLib.inpercent(30,100))
+--print("Current time:", RexLib.getTime())
+--print("Percent:", RexLib.inpercent(25,100))
 
-local tbl = {1,2,3}
-local cloned = RexLib.clonetable(tbl)
-print(cloned)
+--RexLib.wait(5)
+--local tbl = {1,2,3}
+--local cloned = RexLib.clonetable(tbl)
+--print(cloned)
