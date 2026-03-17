46d9e8fd2452515ff2c80a4c5db560bf357030c6
Seb <sebastiankocur21@gmail.com>
2026-03-17 20:55:43 +0100

    work

--- install.bat
+++ install.bat
@@ -1,9 +1,5 @@
 @echo off
-REM Create folder if it doesn't exist
 if not exist "%USERPROFILE%\.rexlib" mkdir "%USERPROFILE%\.rexlib"
-
-REM Download the tagged version
-powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/SebCodesHere/RexLib/1.0-1/rexlib.lua' -OutFile $env:USERPROFILE\.rexlib\rexlib.lua"
-
-echo RexLib 1.0-1 installed to %USERPROFILE%\.rexlib
+powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/SebCodesHere/RexLib/1.0.0/rexlib.lua' -OutFile $env:USERPROFILE\.rexlib\rexlib.lua"
+echo RexLib v1.0.0 installed to %USERPROFILE%\.rexlib
 pause
--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Create folder if it doesn't exist
 mkdir -p ~/.rexlib
-
-# Download the version-specific RexLib
-curl -sL https://raw.githubusercontent.com/SebCodesHere/RexLib/1.0-1/rexlib.lua -o ~/.rexlib/rexlib.lua
-
-echo "RexLib 1.0-1 installed to ~/.rexlib"
+curl -sL https://raw.githubusercontent.com/SebCodesHere/RexLib/1.0.0/rexlib.lua -o ~/.rexlib/rexlib.lua
+echo "RexLib v1.0.0 installed to ~/.rexlib"
