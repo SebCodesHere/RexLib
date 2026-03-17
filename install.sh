a81cb1ceb6759e6c0a3f97560df023c801f8a60b
Seb <sebastiankocur21@gmail.com>
2026-03-17 20:50:24 +0100

    Hope fix

--- install.sh
+++ install.sh
@@ -1,43 +1,4 @@
 #!/bin/bash
-
-VERSION="1.0-1"
-GITHUB_TAG="1.0-1"
-INSTALL_DIR="$HOME/.rexlib"
-GITHUB_URL="https://raw.githubusercontent.com/SebCodesHere/RexLib/$GITHUB_TAG/rexlib.lua"
-
-echo "Installing RexLib v$VERSION..."
-
-# create and go into install dir
-mkdir -p "$INSTALL_DIR"
-cd "$INSTALL_DIR" || exit 1
-
-# download rexlib.lua
-curl -L -O "$GITHUB_URL"
-
-# generate rockspec with exact name LuaRocks expects
-ROCKSPEC="rexlib-$VERSION.rockspec"
-cat > "$ROCKSPEC" <<EOL
-package = "rexlib"
-version = "$VERSION"
-source = { url = "$GITHUB_URL" }
-description = {
-  summary = "RexLib Lua utility functions",
-  detailed = [[
-      RexLib provides utility functions:
-      - getTime()
-      - inpercent(value,maxvalue)
-      - clonetable(TableName)
-  ]],
-  license = "Apache License 2.0"
-}
-dependencies = {}
-build = {
-  type = "builtin",
-  modules = { rexlib = "rexlib.lua" }
-}
-EOL
-
-# install via LuaRocks locally
-luarocks make "$ROCKSPEC" --local
-
-echo "RexLib v$VERSION installed locally! You can now use require('rexlib') anywhere."
+mkdir -p ~/.rexlib
+curl -sL https://raw.githubusercontent.com/SebCodesHere/RexLib/1.0-1/rexlib.lua -o ~/.rexlib/rexlib.lua
+echo "RexLib v1.0-1 installed to ~/.rexlib"
--- random.lua
+++ random.lua
@@ -1,10 +1,13 @@
+-- random.lua
+local home = os.getenv("HOME")
+package.path = package.path .. ";" .. home .. "/.luarocks/share/lua/5.4/?.lua;" .. home .. "/.luarocks/share/lua/5.4/?/init.lua"
+package.cpath = package.cpath .. ";" .. home .. "/.luarocks/lib/lua/5.4/?.so"
+
 local RexLib = require("rexlib")
 
 print("Current time:", RexLib.getTime())
-print("Percent:", RexLib.inpercent(30,100))
+print("Percent example:", RexLib.inpercent(25, 100))
 
-local tbl = {1,2,3}
-local cloned = RexLib.clonetable(tbl)
-for i,v in ipairs(cloned) do
-    print("Cloned value:", v)
-end
+local tbl = {1, 2, 3}
+local clone = RexLib.clonetable(tbl)
+print("Cloned table:", table.concat(clone, ", "))
