b1870b480773c6692b42b8f7fd2f6ce47e8a46ac
Fejtrix <samkodugas2902@gmail.com>
2026-03-17 22:14:24 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,19 +1,13 @@
--- 1. Load the library from the SAME folder
 local success, RexLib = pcall(dofile, "rexlib.lua")
 
 if not success then
-    print("\n--- ERROR IN REXLIB.LUA ---")
-    print(RexLib) -- This prints the actual Lua error (like missing 'end')
     return 
 end
 
--- 2. Check if RexLib actually returned the table
 if type(RexLib) ~= "table" then
-    print("ERROR: rexlib.lua did not 'return rexlib' at the end of the file!")
     return
 end
 
--- 3. TEST FUNCTIONS
 print("Time:", RexLib.getTime())
 print("Percent (25 of 100):", RexLib.inpercent(25, 100))
 
